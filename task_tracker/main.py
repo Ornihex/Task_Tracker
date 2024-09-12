07278521c9967142b1ffd393739dbf899e4a32d1
Ornihex <ornihex@proton.me>
2024-09-12 19:19:51 +1000

    1.0

--- task_tracker/main.py
+++ task_tracker/main.py
@@ -1,6 +1,33 @@
-from tracker import Tracker
-import argparse
+from .tracker import Tracker
+from sys import argv
 
-parser = argparse.ArgumentParser()
-tracker = Tracker()
-tracker.listing()
+def main():
+    tracker = Tracker()
+    if argv[1] == 'add':
+        print(tracker.add(argv[2]))
+    elif argv[1] == 'update':
+        print(tracker.update(argv[2], argv[3]))
+    elif argv[1] == 'delete':
+        print(tracker.delete(argv[2]))
+    elif argv[1] == 'list':
+        try:
+            status = argv[2]
+        except IndexError:
+            status = None
+        print(*tracker.listing(status))
+    elif argv[1][:4] == 'mark':
+        if len(argv[1])>5:
+            mark = argv[1][5:]
+            id = argv[2]
+        else:
+            try:
+                mark = argv[2]
+                id = argv[3]
+            except IndexError:
+                print("""You probably didn't specify a label or ID. Usage examples:
+                                            mark-done <ID>
+                                            mark done <ID>""")
+        print(tracker.mark(id, mark))
+
+if __name__ == '__main__':
+    main()
--- task_tracker/tracker.py
+++ task_tracker/tracker.py
@@ -6,54 +6,75 @@ class Tracker():
         self.check = isfile('tasks.json')
         with open('tasks.json', f'{"r+" if self.check else "w+"}') as file:
             if not self.check:
-                dump({}, file, indent=4)
+                dump({"total": 0}, file, indent=4)
         self.date = datetime.now()
         self.date = f'{self.date.day}.{self.date.month if self.date.month>9 else "0"+str(self.date.month)}.{self.date.year} {self.date.hour}:{self.date.minute}'
     def add(self, task):
         with open("tasks.json", 'r+') as t:
             data = load(t)
-            data[len(data)+1] = {
-                'id': len(data)+1,
-                'description': f"{task}",
-                'status': 0,
+            data['total'] += 1
+            total = data['total']
+            data[total] = {
+                'id': total,
+                'description': task,
+                'status': 'todo',
                 'createdAt': self.date,
                 'updatedAt': self.date
             }
             t.seek(0)
             dump(data, t, indent=4)
-            print(f'Task added succesfully (ID: {len(data)})')
+            return f'Task added succesfully (ID: {total})'
     def update(self, id, text):
+        global data
+        data = {}
         with open('tasks.json', 'r+') as t:
             data = load(t)
-            if len(data) >= id:
-                data[id]['description'] = str(text)
+            if id in data:
+                data[id]['description'] = text
                 data[id]['updatedAt'] = self.date
+                with open('tasks.json', 'w'):
+                    pass
                 t.seek(0)
                 dump(data, t, indent=4)
+                return 'Task updated succesfully!'
             else:
-                print('There is no task with this ID')
+                return 'ERROR: There is no task with this ID'
     def mark(self, id, status):
         with open('tasks.json', 'r+') as t:
             data = load(t)
-            if len(data) >= id:
+            if id in data:
                 data[id]['status'] = status
                 data[id]['updatedAt'] = self.date
+                with open('tasks.json', 'w') as r:
+                    pass
                 t.seek(0)
                 dump(data, t, indent=4)
+                return 'Task marked succesfully!'
             else:
-                print('There is no task with this ID')
+                return 'ERROR: There is no task with this ID'
     def delete(self, id):
         with open('tasks.json', 'r+') as t:
             data = load(t)
             if id in data:
                 data.pop(id)
+                with open('tasks.json', 'w'):
+                    pass
                 t.seek(0)
                 dump(data, t, indent=4)
+                return "Task deleted succesfully!"
             else:
-                print('There is no task with this ID')
+                return 'ERROR: There is no task with this ID'
     def listing(self, status=None):
         with open('tasks.json', 'r+') as t:
             data = load(t)
             if not status:
                 for i in data:
-                    print(f'ID: {data[i]['id']} Status: {data[i]['status']} Description: {data[i]['description']}')
+                    if i == 'total':
+                        continue
+                    yield f'ID: {data[i]['id']} Status: {data[i]['status']} Description: {data[i]['description']}\n'
+            else:
+                for i in data:
+                    if i == 'total':
+                        continue
+                    if data[i]['status'] == status:
+                        yield f'ID: {data[i]['id']} Status: {data[i]['status']} Description: {data[i]['description']}\n'
